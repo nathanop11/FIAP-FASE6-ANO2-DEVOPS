4ef69afd03be4f2434c34b52381b91a0d52169a0
Flavio Moreni <PF0954@fiap.com.br>
2024-05-15 12:36:58 -0300

    viewmodels e mapeamentos

--- Fiap.Api.Alunos/Program.cs
+++ Fiap.Api.Alunos/Program.cs
@@ -1,6 +1,9 @@
+using AutoMapper;
 using Fiap.Web.Alunos.Data.Contexts;
 using Fiap.Web.Alunos.Data.Repository;
+using Fiap.Web.Alunos.Models;
 using Fiap.Web.Alunos.Services;
+using Fiap.Web.Alunos.ViewModel;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,6 +28,47 @@ builder.Services.AddScoped<IRepresentanteService, RepresentanteService>();
 builder.Services.AddScoped<IPedidoService, PedidoService>();
 #endregion
 
+
+#region AutoMapper
+
+// Configuração do AutoMapper
+var mapperConfig = new AutoMapper.MapperConfiguration(c => {
+    // Permite que coleções nulas sejam mapeadas
+    c.AllowNullCollections = true;
+    // Permite que valores de destino nulos sejam mapeados
+    c.AllowNullDestinationValues = true;
+
+    c.CreateMap<ClienteModel, ClienteViewModel>();
+    c.CreateMap<FornecedorModel, FornecedorViewModel>();
+    c.CreateMap<LojaModel, LojaViewModel>();
+    c.CreateMap<PedidoModel, PedidoViewModel>();
+    c.CreateMap<PedidoProdutoModel, PedidoProdutoViewModel>();
+    c.CreateMap<ProdutoModel, ProdutoViewModel>();
+    c.CreateMap<RepresentanteModel, RepresentanteViewModel>();
+    c.CreateMap<ClienteViewModel, ClienteModel>();
+    c.CreateMap<FornecedorViewModel, FornecedorModel>();
+    c.CreateMap<LojaViewModel, LojaModel>();
+    c.CreateMap<PedidoModel, PedidoViewModel>();
+    c.CreateMap<PedidoViewModel, PedidoModel>();
+    c.CreateMap<PedidoProdutoViewModel, PedidoProdutoModel>();
+    c.CreateMap<ProdutoViewModel, ProdutoModel>();
+    c.CreateMap<RepresentanteViewModel, RepresentanteModel>();
+
+
+    c.CreateMap<CreatePedidoViewModel, PedidoModel>()
+            .ForMember(dest => dest.PedidoProdutos, opt => opt.MapFrom(src =>
+                src.ProdutoIds.Select(id => new PedidoProdutoModel { ProdutoId = id }).ToList()));
+
+
+});
+
+// Cria o mapper com base na configuração definida
+IMapper mapper = mapperConfig.CreateMapper();
+
+// Registra o IMapper como um serviço singleton no container de DI do ASP.NET Core
+builder.Services.AddSingleton(mapper);
+
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
